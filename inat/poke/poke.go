04070e866f666dd3e9e643a0068dee17e0f7d597
Sameer Ajmani <ajmani@gmail.com>
2025-07-28 16:35:31 -0400

    Image upload works!

--- inat/poke/poke.go
+++ inat/poke/poke.go
@@ -7,8 +7,10 @@ import (
 	"io"
 	"log"
 	"mime"
+	"mime/multipart"
 	"net/http"
 	"os"
+	"path"
 	"strings"
 
 	"github.com/Sajmani/birdsync/inat"
@@ -17,61 +19,135 @@ import (
 
 const UserAgent = "birdsync-testing/0.1"
 
+func usage() {
+	fmt.Print(`poke [create|image]
+poke image <source URL> <dest observation UUID>
+`)
+	os.Exit(1)
+}
+
 func main() {
+	if len(os.Args) < 2 {
+		usage()
+	}
 	switch os.Args[1] {
-	case "fetch":
-		downloadImage(os.Args[2])
 	case "create":
 		createTestObservation(getAPIToken())
+	case "image":
+		if len(os.Args) < 4 {
+			usage()
+		}
+		imageURL := os.Args[2]
+		obsUUID := os.Args[3]
+		filename, err := downloadImage(imageURL)
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = uploadImage(getAPIToken(), filename, obsUUID)
+		if err != nil {
+			log.Fatal(err)
+		}
+	default:
+		usage()
 	}
 }
 
 // downloadImage downloads an image from a given URL and saves it to a specified file path.
-func downloadImage(url string) error {
+func downloadImage(url string) (string, error) {
 	resp, err := http.Get(url)
 	if err != nil {
-		return fmt.Errorf("failed to make HTTP request: %w", err)
+		return "", fmt.Errorf("failed to make HTTP request: %w", err)
 	}
 	defer resp.Body.Close() // Close the response body when the function exits
 	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("bad status code: %s", resp.Status)
+		return "", fmt.Errorf("bad status code: %s", resp.Status)
 	}
 
 	tmpFile, err := os.CreateTemp("", "birdsync")
 	if err != nil {
-		log.Fatal(err)
+		return "", fmt.Errorf("failed to create temp file: %w", err)
 	}
 	_, err = io.Copy(tmpFile, resp.Body)
 	if err != nil {
-		return fmt.Errorf("failed to copy image data to file: %w", err)
+		return "", fmt.Errorf("failed to copy image data to file: %w", err)
 	}
 
 	// Re-open the file to detect content type
 	_, err = tmpFile.Seek(0, 0)
 	if err != nil {
-		return fmt.Errorf("failed to seek to beginning of temp file: %w", err)
+		return "", fmt.Errorf("failed to seek to beginning of temp file: %w", err)
 	}
 
 	buf := make([]byte, 512) // 512 bytes is the required size for DetectContentType
 	n, err := tmpFile.Read(buf)
 	if err != nil && err != io.EOF {
-		return fmt.Errorf("failed to read from temp file for content type detection: %w", err)
+		return "", fmt.Errorf("failed to read from temp file for content type detection: %w", err)
 	}
 	buf = buf[:n]
 
 	mimeType := http.DetectContentType(buf)
 	extensions, err := mime.ExtensionsByType(mimeType)
 	if err != nil || len(extensions) == 0 {
-		return fmt.Errorf("failed to find file extension for mime type %s: %w", mimeType, err)
+		return "", fmt.Errorf("failed to find file extension for mime type %s: %w", mimeType, err)
 	}
 	tmpFile.Close() // Close the file before renaming it.
 
 	newPath := tmpFile.Name() + extensions[0]
 	err = os.Rename(tmpFile.Name(), newPath)
 	if err != nil {
-		return fmt.Errorf("failed to rename file: %w", err)
+		return "", fmt.Errorf("failed to rename file: %w", err)
 	}
 	fmt.Printf("Image downloaded and saved to %s\n", newPath)
+	return newPath, nil
+}
+
+func uploadImage(apiToken string, filename string, obsUUID string) error {
+	var requestBody bytes.Buffer
+	writer := multipart.NewWriter(&requestBody)
+	fileWriter, err := writer.CreateFormFile("file", path.Base(filename))
+	if err != nil {
+		return err
+	}
+	f, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	_, err = io.Copy(fileWriter, f)
+	if err != nil {
+		return err
+	}
+	err = writer.WriteField("observation_photo[observation_id]", obsUUID)
+	if err != nil {
+		return err
+	}
+	err = writer.Close()
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest("POST", "https://api.inaturalist.org/v2/observation_photos", &requestBody)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("User-Agent", UserAgent)
+	req.Header.Set("Authorization", apiToken)
+
+	// Set the Content-Type header to the multipart writer's boundary.
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+
+	//fmt.Printf("\nREQUEST: %+v\n", req)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//fmt.Printf("\nRESPONSE: %+v\n", resp)
+	b, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("\nBODY: " + string(b))
+
 	return nil
 }
 
@@ -95,6 +171,7 @@ func createTestObservation(apiToken string) {
 	}
 	req.Header.Set("User-Agent", UserAgent)
 	req.Header.Set("Authorization", apiToken)
+
 	fmt.Printf("\nREQUEST: %+v\n", req)
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
