9f64cb03380fc54b6b3bba701cf93e4968a5b0e2
Sameer Ajmani <sameer@golang.org>
2025-07-30 12:31:05 +0200

    Update birdsync.go

--- birdsync.go
+++ birdsync.go
@@ -1,5 +1,9 @@
 // Birdsync syncs eBird observations and photos to iNaturalist.
 //
+// As a prerequisite, you must download your data from eBird using
+// https://ebird.org/downloadMyData — save the zip file and unzip it
+// to get the MyEBirdData.csv file.
+// 
 // Birdsync works as follows:
 //  1) download all iNaturalist observations for INAT_USER_ID into memory
 //  2) index these observations by (eBird checklist ID, species name)
